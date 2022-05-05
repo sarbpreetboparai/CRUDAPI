3edb9407cddf70e7814452b3545cd7d9ce402d65
Sarbpreet Boparai <boparai.sarbpreet@gmail.com>
2022-05-05 15:10:12 -0400

    Sarb

---
+++ CRUDAPI/Controller/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRUDAPI.Controller
+{
+    [BasicAuth]
+    public class HomeController : ApiController
+    {
+        private SynCustoms entities = new SynCustoms();
+        [HttpGet]
+        public HttpResponseMessage GetCustomer()
+        {
+            List<Customer> result = new List<Customer>();
+            result = entities.Customer.ToList();
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+
+        }
+    }
+}
